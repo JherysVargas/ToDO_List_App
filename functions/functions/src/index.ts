21749c16aa30d8a9436e317ae8078567819d3b6c
Jherys Vargas <eduardo_9615@hotmail.com>
2023-12-17 09:27:33 -0500

    translation improvements

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -1,14 +1,17 @@
-import { onDocumentCreated } from "firebase-functions/v2/firestore";
-import type { DocumentData } from "firebase-admin/firestore";
-import { TranslationServiceClient } from "@google-cloud/translate";
+import {onDocumentCreated} from "firebase-functions/v2/firestore";
+import {TranslationServiceClient} from "@google-cloud/translate";
+import type {
+  DocumentData,
+  QueryDocumentSnapshot,
+} from "firebase-admin/firestore";
 
 const projectId = "todo-list-2382b";
-const translationClient = new TranslationServiceClient();
 
 const keys: string[] = ["title", "description"];
 
 exports.createTask = onDocumentCreated("tasks/{taskId}", async (event) => {
-  const snapshot = event.data;
+  const translationClient = new TranslationServiceClient();
+  const snapshot: QueryDocumentSnapshot | undefined = event.data;
   if (!snapshot) {
     return;
   }
@@ -26,9 +29,9 @@ exports.createTask = onDocumentCreated("tasks/{taskId}", async (event) => {
   if (response?.translations?.length !== 0) {
     const translations = response.translations ?? [];
     const translatedTask = Object.fromEntries(
-      keys.map((key, index) => [key, translations[index]?.translatedText])
+        keys.map((key, index) => [key, translations[index]?.translatedText])
     );
 
-    await snapshot.ref.set({ translate: translatedTask }, { merge: true });
+    await snapshot.ref.set({translation: translatedTask}, {merge: true});
   }
 });
